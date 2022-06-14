b7bd723821df0376fb06212f6897927c17df86b6
openfnord <83826561+openfnord@users.noreply.github.com>
2022-06-14 08:44:34 +0000

    Update inst.sh
    I wrote this using the browser, without trying on shell :-(

--- inst.sh
+++ inst.sh
@@ -4,7 +4,7 @@ mkdir ~/.pandoc
 mkdir ~/.pandoc/templates/
 cp ./letter-template.tex ~/.pandoc/templates/
 
-echo "installing wush.sh ("very small make pdf script") to ~/bin
+echo "installing wush.sh (very small make pdf script) to ~/bin
 cp ./wush.sh ~/bin
 
 echo "if it fails have ~/bin, include it into your path or ask root to copy it to /usr/local/bin"
