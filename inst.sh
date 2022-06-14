8ac884b0b1bada78d47d0dd2754d680bbae0c3e6
openfnord <83826561+openfnord@users.noreply.github.com>
2022-06-14 08:42:54 +0000

    created inst.sh
    created a very simple installer so you dont need to look up where to put the templates in pandoc...

---
+++ inst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "installing template to ~/.pandoc/templates"
+mkdir ~/.pandoc
+mkdir ~/.pandoc/templates/
+cp ./letter-template.tex ~/.pandoc/templates/
+
+echo "installing wush.sh ("very small make pdf script") to ~/bin
+cp ./wush.sh ~/bin
+
+echo "if it fails have ~/bin, include it into your path or ask root to copy it to /usr/local/bin"
+
+
+
